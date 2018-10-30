691d543315ad2633a9b168d2489b61e07bf50234
Sali Safi <sali_safi@yahoo.com>
2018-10-30 02:54:58 +0000

    first commit after team meeting

--- Group-08/index.js
+++ Group-08/index.js
@@ -166,7 +166,20 @@ app.post('/userProfile', function(req, res) {
 		}
     }
   });
-}); 
+});
+
+app.post('/postItem', function (req, res) {
+  var body = req.body;
+
+  console.log("Post Item is clicked!!!");
+
+  connection.query("INSERT INTO testTbl(name, description) VALUES (?,?)", [
+	  body.name, body.description
+    ], function() {
+    res.write('success');
+    res.end();
+  });
+});
 
 /*************** 404 Not Found **************/
 
